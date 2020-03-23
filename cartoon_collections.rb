875012b04beab224cb08138787bd61ad5b02d7e4
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-23 23:40:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(arr)
   arr.collect {|el| el[0].upcase! + el[1..el.length-1] + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
