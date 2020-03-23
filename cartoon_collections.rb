658e12da53e451726734a865b70e9d559c47bbde
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-23 23:39:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.collect {|el| el.upcase! + "!"}
+  arr.collect {|el| el[0].upcase! + el[1..el.length-1] + "!"}
 end
 
 def long_planeteer_calls# code an argument here
